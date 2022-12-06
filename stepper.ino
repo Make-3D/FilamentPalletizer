f9475d5b3a29bb1cb27fdd7c57f171d36c73a1af
Make-3D <baboucarry@gmail.com>
2022-12-06 11:30:46 +0000

    Formated the code properly

--- stepper.ino
+++ stepper.ino
@@ -23,48 +23,4 @@ void loop() {
     digitalWrite(stepPin, LOW);
     delayMicroseconds(5000);
   }
-
-//  delay(1000);
-//
-//  // Set the spinning direction counterclockwise:
-//  digitalWrite(dirPin, LOW);
-//
-//  // Spin the stepper motor 1 revolution quickly:
-//  for (int i = 0; i < stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(1000);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(1000);
-//  }
-//
-//  delay(1000);
-//
-//  // Set the spinning direction clockwise:
-//  digitalWrite(dirPin, HIGH);
-//
-//  // Spin the stepper motor 5 revolutions fast:
-//  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(500);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(500);
-//  }
-//
-//  delay(1000);
-//
-//  // Set the spinning direction counterclockwise:
-//  digitalWrite(dirPin, LOW);
-//
-//  //Spin the stepper motor 5 revolutions fast:
-//  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(500);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(500);
-//  }
-//
-//  delay(1000);
 }
